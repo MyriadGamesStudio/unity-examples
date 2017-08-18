d4e4f22eb81e304cb68cd2d342d1c126eda85c28
Jeremiah Walter <jeremiah.walter@rxpservices.com>
2017-08-18 16:22:50 +1000

    adding singleton and synchronous sceneload methods

---
+++ Assets/SceneManager/Scripts/MenuFunctions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MenuFunctions : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneLoader.Instance.DisplayLoadingScene();
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/SceneManager/Scripts/SceneLoadManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : Singleton<SceneLoader>
+{
+    // guarantee this will be always a singleton only - can't use the constructor!
+    protected SceneLoader() { }
+
+    #region synchronous methods
+
+    public void DisplayLoadingScene()
+    {
+        SceneManager.LoadScene("Loading");
+
+        StartCoroutine(LoadAfterTimer());
+    }
+
+    private IEnumerator LoadAfterTimer()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        LoadScene();
+    }
+
+    public void LoadScene(string sceneName = "Game")
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    #endregion
+}
